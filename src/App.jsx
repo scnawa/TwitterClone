db3b4912893c712152e7d98e2ca3fe7ba99ca11b
scnawa <51161576+scnawa@users.noreply.github.com>
2023-12-05 19:36:13 +1100

    Doing initial setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import './sidebar.css'
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+      NotTwitter
+      {/** Feed */}
+
+      {/** Ads */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <TwitterIcon/>
+        <HomeIcon/>
+        {/* Sidebar Option */}
+        {/* Sidebar Option */}
+    </div>
+  )
+}
+
+export default Sidebar
