1078980334072d9492e375d442ede7ae8b5a7a2f
scnawa <51161576+scnawa@users.noreply.github.com>
2023-12-08 13:33:28 +1100

    sidebar completed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import HomeIcon from '@mui/icons-material/Home';
-import TwitterIcon from '@mui/icons-material/Twitter';
+  import React from 'react'
+  import SidebarOption from './SidebarOption';
+  import TwitterIcon from '@mui/icons-material/Twitter';
+  import HomeIcon from '@mui/icons-material/Home';
+  import SearchIcon from '@mui/icons-material/Search';
+  import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+  import MailOutlineIcon from '@mui/icons-material/MailOutline';
+  import BookmarkIcon from '@mui/icons-material/Bookmark';
+  import ListAltIcon from '@mui/icons-material/ListAlt';
+  import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+  import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+  import { Button } from '@mui/material';
 
-function Sidebar() {
-  return (
-    <div className='sidebar'>
-        <TwitterIcon/>
-        <HomeIcon/>
-        {/* Sidebar Option */}
-        {/* Sidebar Option */}
-    </div>
-  )
-}
+  function Sidebar() {
+    return (
+      <div className='sidebar'>
+          <TwitterIcon className="sidebarTwitterIcon"/>
+          <SidebarOption active Icon={HomeIcon} text="Home" />
+          <SidebarOption Icon={SearchIcon} text="Explore" />
+          <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
+          <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+          <SidebarOption Icon={BookmarkIcon} text="Bookmarks" />
+          <SidebarOption Icon={ListAltIcon} text="Lists" />
+          <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+          <SidebarOption Icon={MoreHorizIcon} text="More" />
 
-export default Sidebar
+          <Button variant="outlined" className="sidebaTweet" fullWidth>
+            Tweet
+          </Button>
+      </div>
+    )
+  }
+
+  export default Sidebar
